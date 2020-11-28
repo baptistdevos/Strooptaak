60e90ed5f84bad44dc236176c00e1718ff9767c0
baptistdevos <75160762+baptistdevos@users.noreply.github.com>
2020-11-28 21:47:27 +0100

    Update Klad_STROOP_TASK.py

--- Klad_STROOP_TASK.py
+++ Klad_STROOP_TASK.py
@@ -5,8 +5,27 @@ Created on Sat Nov 28 09:53:11 2020
 @author: Baptist Devos
 """
 
-## TEST TEST 
+### Alles importeren
+
+##Import everything needed
+
+import pip
+import math #maths
+import pandas as pd #data from other programs
+
+import psychopy
+from psychopy import visual, event, core
+import psychopy.visual
+
+#visual screen
+
+import psychopy.visual
+
+#openen van een screen
+
+win = psychopy.visual.Window(size=[400, 400], units="pix", fullscr=False, color=[1, 1, 1])
+
+# Trying out a static stimulus
 
 
-## 2de TEST 
 
